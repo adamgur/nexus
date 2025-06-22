5b6efa371619e8d8d8c3bcc9c414affa0094ad93
adamgur <adamgurevitz1@gmail.com>
2025-06-22 17:24:47 +0300

    fix(): move the gateway outputs to outputs file

---
+++ terraform/modules/gateway/outputs.tf
@@ -0,0 +1,19 @@
+output "application_gateway_public_ip" {
+  value = azurerm_public_ip.agic_public_ip.ip_address
+  description = "The public IP address of the Azure Application Gateway."
+}
+
+output "aks_subnet_id" {
+  value       = azurerm_subnet.aks_subnet.id
+  description = "The ID of the subnet dedicated for AKS."
+}
+
+output "key_vault_uri" {
+  value       = azurerm_key_vault.main_kv.vault_uri
+  description = "The URI of the main Azure Key Vault."
+}
+
+output "key_vault_name" {
+  value       = azurerm_key_vault.main_kv.name
+  description = "The name of the main Azure Key Vault."
+}
